ae53998e571b34cd1db71c1afd03b9c7b9a51b60
M4st3rK0mb4c <carlos.g.m.1103@gmail.com>
2024-12-10 11:02:40 +0100

    tema13 GBD

--- Grado Superior/segundo/GBD/Solucion_practicas_bloque3_Temas_9_10_11_12.sql
+++ Grado Superior/segundo/GBD/Solucion_practicas_bloque3_Temas_9_10_11_12.sql
@@ -1686,7 +1686,7 @@ mysql.user
 3. Mostrar usuario, host y password de todas las cuentas de ususario.
 */
 
-
+select user, host, authetication_string from mysql.user;
 
 
 
@@ -1698,7 +1698,8 @@ mysql.user
 modo local con todos los privilegios o permisos sobre todas las tablas de todas las BBDD.
 */
 
-
+GRANT ALL PRIVILEGES ON *.* 
+    TO usuario1@localhost IDENTIFIED BY 'usuario1';
 
 
 
@@ -1711,7 +1712,8 @@ con el permiso SELECT sobre todas las tablas de todas las BBDD.
 */
 
 
-
+GRANT ALL PRIVILEGES ON *.* 
+    TO usuario2@localhost IDENTIFIED BY 'usuario2';
 
 
 
@@ -1733,7 +1735,7 @@ tiene el hostname?
 */
 
 
-
+SELECT USER, HOST, authetication_string FROM MYSQL.USER;
 
 
 
@@ -1761,3 +1763,144 @@ los siguietes campos: select_priv, insert_priv y c. delete_priv.
 
 
 
+
+
+
+
+
+                                               /*==================================================================================*/
+                                                /*==================================================================================*/
+                                                /*=================================  PRACTICA 2 TEMA 13  ===========================*/
+                                                /*==================================================================================*/
+                                                /*==================================================================================*/
+                                                                /*Apuntes bloque III desde la página 44 a la página 49.*/
+
+
+
+/* 1. Conectarte localmente con el usuario "root" de forma corta y después comprobar el usuario con el que has entrado al servidor. */
+
+
+
+/* 2. Crear una cuenta de usuario llamada "usuario3" y contraseña "user3" para acceder en modo local con todos los privilegios o permisos sobre todas las tablas de la BBDD “curso”. */
+
+
+
+
+
+
+
+
+/* 3. Crear una cuenta de usuario llamada "usuario4" sin contraseña para acceder en modo local con el permiso SELECT,INSERT sobre todas las tablas de la BBDD “curso”. */
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/* 4. Mostrar los campos user, host y password de la tabla user de la BBDD mysql. */
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/* 5. Mostrar los campos host, db y user de la tabla db de la BBDD mysql. */
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/* 6. Como sabes el “usuario4” se ha creado sin contraseña. Deseamos asignarle la contraseña "usuario4".  ¿Cuál/es de las siguientes formas es válida?. Para la/las formas válidas debes indicar todos los pasos.
+a) Conectado desde el usuario3. 
+b) Conectado desde el usuario4.
+c) Conectado desde el usuario1. */
+
+
+
+
+
+
+
+/* 7. Como sabes el “usuario3” se ha creado con contraseña. Deseamos asignarle una nueva contraseña "usuario3".  ¿Cuál/es de las siguientes formas es válida?. Para la/las formas válidas debes indicar todos los pasos. 
+a) Conectado desde el usuario3. 
+b) Conectado desde el usuario4. */
+
+
+
+
+
+
+
+
+
+
+/* 8. Desconectarte como usuario root. Volver a conectarte localmente con el usuario "usuario3" de forma corta y comprobar el usuario con el que has entrado al servidor. */
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/* 9. Muestra   únicamente   los   privilegios   o   permisos   que   tiene   asignado   el   usuario 
+"usuario3@localhost". */
+
+
+
+
+
+
+
+
+
+
+
+
+
+/* 10. Desde "usuario3@localhost" crea una nueva BBDD llamada "instituto". Si no es posible 
+explica por qué.  */
